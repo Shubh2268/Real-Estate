643f6dbe9bca0703861cd878d0be513d4b60fcb6
Shubh <shubh2268@gmail.com>
2024-02-10 17:23:42 +0530

    --

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -11,13 +11,13 @@ const Categories = () => {
 
       <div className='h-screen my-14 mx-4 md:mx-10'>
         <div className='grid grid-cols-1 grid-rows-3 md:grid-rows-4 md:grid-cols-4 gap-3 md:gap-4 h-screen'>
-          <div className='bg-indigo-100 col-span-1 md:row-span-2 md:col-span-2 w-full bg-center bg-cover rounded-md' style={{backgroundImage: `url('https://imagetolink.com/ib/cPyUD6C0ZY.jpg')`}}>
+          <div className='col-span-1 md:row-span-2 md:col-span-2 w-full bg-center bg-cover rounded-md' style={{backgroundImage: `url('https://imagetolink.com/ib/cPyUD6C0ZY.jpg')`}}>
             <span className='text-white fixed bottom-2'>Penthouse</span>
           </div>
-          <div className='bg-fuchsia-100 col-span-1 md:col-span-2 row-span-1 md:row-span-4 w-full bg-center bg-cover rounded-md' style={{backgroundImage: `url('https://imagetolink.com/ib/cPyUD6C0ZY.jpg')`}}>
+          <div className='col-span-1 md:col-span-2 row-span-1 md:row-span-4 w-full bg-center bg-cover rounded-md' style={{backgroundImage: `url('https://imagetolink.com/ib/cPyUD6C0ZY.jpg')`}}>
             <span className='text-white fixed bottom-2'>Off Plan Property</span>
           </div>
-          <div className='bg-purple-100 col-span-1 md:row-span-2 md:col-span-2 w-full bg-center bg-cover rounded-md' style={{backgroundImage: `url('https://imagetolink.com/ib/cPyUD6C0ZY.jpg')`}}>
+          <div className='col-span-1 md:row-span-2 md:col-span-2 w-full bg-center bg-cover rounded-md' style={{backgroundImage: `url('https://imagetolink.com/ib/cPyUD6C0ZY.jpg')`}}>
             <span className='text-white fixed bottom-2'>Rented Apartment</span>
           </div>
         </div>
